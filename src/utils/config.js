265e77a3867e180f016a201395b9948a182d1463
dafuentes <darifuentes@gmail.com>
2023-08-23 00:21:31 -0600

    Set baseurl production

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const development = true;
+const development = false;
 const BASE_URL = development
   ? "http://127.0.0.1:3001"
   : "https://clean-wonder-395819.uc.r.appspot.com";
